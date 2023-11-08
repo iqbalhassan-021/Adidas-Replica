1903e09a8e1b3a2a546b3336e7e3716951c31f85
Iqbal Hassan <102966946+iqbalhassan-021@users.noreply.github.com>
2023-11-08 20:39:55 +0500

    commit

---
+++ codes/code.js
@@ -0,0 +1,55 @@
+var counter = 1;
+
+function dispnavBar(){
+    counter = counter + 1;
+    if(counter%2 == 0){
+        document.getElementById('mobileNavBar').style.display = "none";
+    }
+    else{
+        document.getElementById('mobileNavBar').style.display = "block";
+    }
+}
+
+
+function nextSlide(){
+    counter = counter + 1;
+    if(counter==1){
+        document.getElementById('container1').scrollIntoView();  
+    }
+    if(counter==2){
+        document.getElementById('container2').scrollIntoView();  
+    }
+    if(counter==3){
+        document.getElementById('container3').scrollIntoView();  
+    }
+    if(counter==4){
+        document.getElementById('container4').scrollIntoView();  
+    }
+    if(counter>4){
+        counter = 1;
+        document.getElementById('container1').scrollIntoView();  
+    }
+}
+
+
+function preSlide(){
+    counter = counter - 1;
+    if(counter==1){
+        document.getElementById('container1').scrollIntoView();  
+    }
+    if(counter==2){
+        document.getElementById('container2').scrollIntoView();  
+    }
+    if(counter==3){
+        document.getElementById('container3').scrollIntoView();  
+    }
+    if(counter==4){
+        document.getElementById('container4').scrollIntoView();  
+    }
+    if(counter<0){
+        counter = 1;
+        document.getElementById('container1').scrollIntoView();  
+    }
+}
+
+
