1cfa0bc13e71879946d15791bcb6eff96cfbc63e
Iqbal Hassan <102966946+iqbalhassan-021@users.noreply.github.com>
2023-11-29 13:05:40 +0500

    commit

--- codes/code.js
+++ codes/code.js
@@ -1,6 +1,5 @@
 var counter = 1;
-var countSlider = 1;
-var blogCounter = 1;
+
 
 function dispnavBar() {
     counter = counter + 1;
@@ -17,49 +16,6 @@ function HideUpdates() {
     document.getElementById('newupdates').style.display = "none";
 }
 
-function nextSlide() {
-    countSlider = countSlider + 1;
-    if (countSlider == 1) {
-        document.getElementById('container1').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-
-    }
-    else if (countSlider == 2) {
-        document.getElementById('container2').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider == 3) {
-        document.getElementById('container3').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider == 4) {
-        document.getElementById('container4').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider > 4 || countSlider < 0) {
-        countSlider = 1;
-        document.getElementById('container1').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    console.log(countSlider);
-
-}
-
-
-function preSlide() {
-    countSlider = countSlider - 1;
-    if (countSlider == 1) {
-        document.getElementById('container1').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider == 2) {
-        document.getElementById('container2').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider == 3) {
-        document.getElementById('container3').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider == 4) {
-        document.getElementById('container4').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-    else if (countSlider > 4 || countSlider < 0) {
-        countSlider = 1;
-        document.getElementById('container1').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-}
 
 function closePopUp() {
     document.getElementById('bluredback').style.display = "none";
@@ -85,36 +41,3 @@ function ShowPopUp() {
     document.getElementById('popup').style.display = "block";
 
 }
-function blognextSlide() {
-    blogCounter = blogCounter + 1;
-    if (blogCounter == 1) {
-        document.getElementById('blogcounter5').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });;
-        document.getElementById('boxes1').style.backgroundColor = "rgba(255, 255, 255, 0.425)";
-        document.getElementById('boxes2').style.backgroundColor = "white";
-    }
-    else if (blogCounter == 2) {
-        document.getElementById('blogcounter8').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-        document.getElementById('boxes1').style.backgroundColor = "rgba(255, 255, 255, 0.425)";
-        document.getElementById('boxes2').style.backgroundColor = "white";
-
-
-    }
-    else if (blogCounter > 2 || blogCounter < 1) {
-        blogCounter = 1;
-        document.getElementById('boxes1').style.backgroundColor = "white";
-        document.getElementById('boxes2').style.backgroundColor = "rgba(255, 255, 255, 0.425)";
-        document.getElementById('boxes3').style.backgroundColor = "rgba(255, 255, 255, 0.425)";
-        document.getElementById('blogcounter2').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-
-}
-function blogpreSlide() {
-
-    blogCounter = 1;
-    document.getElementById('blogcounter1').scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    document.getElementById('boxes1').style.backgroundColor = "white";
-    document.getElementById('boxes2').style.backgroundColor = "rgba(255, 255, 255, 0.425)";
-    document.getElementById('boxes3').style.backgroundColor = "rgba(255, 255, 255, 0.425)";
-
-}
-
